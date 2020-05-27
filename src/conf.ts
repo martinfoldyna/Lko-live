3b59d0cf1d1e2c398506034a95db5b6122215e41
Martin Foldyna 1 <>
2020-05-27 15:22:35 +0200

    Github pages

--- src/conf.ts
+++ src/conf.ts
@@ -4,7 +4,8 @@ export const Config = {
     scopes: ["user.read"]
   },
   google: {
-    providerID: '752176834537-n6n0f6q82e1v69kv1q333fc7m0ubkue2.apps.googleusercontent.com', // Klíč pro Google přihlášení
+    providerID: '63183198040-f4ou24pt4uoer9r7cme3tcg6uukt5bkb.apps.googleusercontent.com', // Klíč pro Google přihlášení
+    // providerID: '752176834537-n6n0f6q82e1v69kv1q333fc7m0ubkue2.apps.googleusercontent.com', // PSLIB - Klíč pro Google přihlášení
   },
   apiUrl: 'https://lyceum.pslib.cloud/api/', // URL k api
 }
